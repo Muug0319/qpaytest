f38e5310270a6c3e7f31104270f57f041b5bf6ca
Muug0319 <71243789+Muug0319@users.noreply.github.com>
2020-10-11 17:49:42 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,71 +1,83 @@
-<!-- This is a static file -->
-<!-- served from your routes in server.js -->
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="A cool thing made with Glitch">
+<html>
+<head>
+	<h1>
+		Та төлбөр төлөх банкаа сонгоно уу!
+	</h1>
+</head>
+<body>
 
-    <title>BZP Test</title>
+<a data-app="<?php echo $_GET['khanbank'];?>" data-store-android="mn.slide.khaanbank" data-store-ios="610317572" href="<?php echo $_GET['khanbank'];?>">
+    <img src="img/khanbank.jpg" alt="khanbank">
+</a>
 
-    <link id="favicon" rel="icon" href="https://glitch.com/edit/favicon-app.ico" type="image/x-icon">
-    
-  </head>
-  <body>
-    <script>
-      (function(d, s, id)
-       {
-        var js, fjs = d.getElementsByTagName(s)[0];
-        if (d.getElementById(id)) {return;}
-        js = d.createElement(s); js.id = id;
-        js.src = "//connect.facebook.net/en_US/messenger.Extensions.js";
-        fjs.parentNode.insertBefore(js, fjs);
-      }
-       (document, 'script', 'Messenger'));
-    </script>
-    
-    <a data-app="<?php echo $_GET['khanbank'];?>" data-store-android="mn.slide.khaanbank" data-store-ios="610317572" href="<?php echo $_GET['khanbank'];?>">
-    <img src="Qpay-Test/img/khanbank.jpg" alt="khanbank">
-	</a>
+<ul>
+      <li>
+        <a data-app="00020101021215312794049627940496000000000000BZP52047399530349654031005802MN5915BI_ZET PI KHKHK6011Ulaanbaatar6244010712345670504test0721zMogbxQIPaDIyRWJFuIgf63041BED" data-store-android="mn.slide.khaanbank" data-store-ios="610317572" href="khanbank://q?qPay_QRcode=00020101021215312794049627940496000000000000BZP52047399530349654031005802MN5915BI_ZET PI KHKHK6011Ulaanbaatar6244010712345670504test0721c4xdqhI3SvHL4Y1xSGrfz63046B04">
+          <img src="https://wordpress-287515-1408152.cloudwaysapps.com/wp-content/uploads/2020/09/k1.png" alt="khanbank">
+          <span>Khan Bank</span>
+        </a>
+      </li>
+      <li>
+        <a data-app="tdbbank://q?qPay_QRcode=7729010220721857815534997762133508" data-store-android="mn.tdb.pay" data-store-ios="1458831706" href="tdbbank://q?qPay_QRcode=7729010220721857815534997762133508">
+          <img src="https://wordpress-287515-1408152.cloudwaysapps.com/wp-content/uploads/2020/09/k2.png" alt="tdbbank">
+          <span>TDB Online</span>
+        </a>
+      </li>
+      <li>
+        <a data-app="statebank://q?qPay_QRcode=7729010220721857815534997762133508" data-store-android="mn.statebank.mobilebank" data-store-ios="703343972" href="statebank://q?qPay_QRcode=7729010220721857815534997762133508">
+          <img src="https://wordpress-287515-1408152.cloudwaysapps.com/wp-content/uploads/2020/09/k3.png" alt="statebank">
+          <span>State Bank</span>
+        </a>
+      </li>
+      <li>
+        <a data-app="xacbank://q?qPay_QRcode=7729010220721857815534997762133508" data-store-android="mn.xacbank.smartbank" data-store-ios="781439021" href="xacbank://q?qPay_QRcode=7729010220721857815534997762133508">
+          <img src="https://wordpress-287515-1408152.cloudwaysapps.com/wp-content/uploads/2020/09/k4.jpg" alt="xacbank">
+          <span>XacBank</span>
+        </a>
+      </li>
+      <li>
+        <a data-app="capitronbank://q?qPay_QRcode=7729010220721857815534997762133508" data-store-android="mn.ecapitron" data-store-ios="1312706504" href="capitronbank://q?qPay_QRcode=7729010220721857815534997762133508">
+          <img src="https://wordpress-287515-1408152.cloudwaysapps.com/wp-content/uploads/2020/09/k5.png" alt="capitronbank">
+          <span>Capitron Bank</span>
+        </a>
+      </li>
+      <li>
+        <a data-app="ckbank://q?qPay_QRcode=7729010220721857815534997762133508" data-store-android="mn.ckbank.smartbank2" data-store-ios="1180620714" href="ckbank://q?qPay_QRcode=7729010220721857815534997762133508">
+          <img src="https://wordpress-287515-1408152.cloudwaysapps.com/wp-content/uploads/2020/09/k6.png" alt="ckbank">
+          <span>Chinggis khaan</span>
+        </a>
+      </li>
+      <li>
+        <a data-app="bogdbank://q?qPay_QRcode=7729010220721857815534997762133508" data-store-android="com.bogdbank.ebank.v2" data-store-ios="1475442374" href="bogdbank://q?qPay_QRcode=7729010220721857815534997762133508">
+          <img src="https://wordpress-287515-1408152.cloudwaysapps.com/wp-content/uploads/2020/09/k7.jpg" alt="bogdbank">
+          <span>Bogd Bank</span>
+        </a>
+      </li>
+      <li>
+        <a data-app="nibank://q?qPay_QRcode=7729010220721857815534997762133508" data-store-android="mn.nibank.mobilebank" data-store-ios="882075781" href="nibank://q?qPay_QRcode=7729010220721857815534997762133508">
+          <img src="https://wordpress-287515-1408152.cloudwaysapps.com/wp-content/uploads/2020/09/k8.jpeg" alt="nibank">
+          <span>NIBank</span>
+        </a>
+      </li>
+      <li>
+        <a data-app="most://q?qPay_QRcode=7729010220721857815534997762133508" data-store-android="mn.grapecity.mostmoney" data-store-ios="487144325" href="most://q?qPay_QRcode=7729010220721857815534997762133508">
+          <img src="https://wordpress-287515-1408152.cloudwaysapps.com/wp-content/uploads/2020/09/k9.png" alt="most">
+          <span>MostMoney</span>
+        </a>
+      </li>
+      <li>
+        <a onclick="loadQpayWallet()">
+          <img src="https://wordpress-287515-1408152.cloudwaysapps.com/wp-content/uploads/2020/09/kq.png" alt="qpay wallet">
+          <span>QPAY wallet</span>
+        </a>
+      </li>
+<li>
+        <a data-app="khanbank://q?qPay_QRcode=00020101021215312794049627940496000000000000BZP52047399530349654031005802MN5915BI_ZET PI KHKHK6011Ulaanbaatar6244010712345670504test0721c4xdqhI3SvHL4Y1xSGrfz63046B04" data-store-android="mn.slide.khaanbank" data-store-ios="610317572" href="khanbank://q?qPay_QRcode=00020101021215312794049627940496000000000000BZP52047399530349654031005802MN5915BI_ZET PI KHKHK6011Ulaanbaatar6244010712345670504test0721c4xdqhI3SvHL4Y1xSGrfz63046B04">
+          <img src="https://wordpress-287515-1408152.cloudwaysapps.com/wp-content/uploads/2020/09/k1.png" alt="khanbank">
+          <span>Khan Bank</span>
+        </a>
+      </li>
+    </ul>
 
-    <script>
-      window.onload = function( {
-        var khanbank = url.khanbank("khanbank");
-      })
-    </script>
-     
-    
-    
-    <script>
-    window.extAsyncInit = function() {
-      console.log('Messenger extensions are ready');
-      
-      $('#registrationForm').submit(function(event) {
-        console.log('Submit pressed');
-        
-        event.preventDefault();
-        
-        const formData = $('#registrationForm').serialize();
-        
-        $.post('/broadcast-to-manychat', formData, function (data) {
-          MessengerExtensions.requestCloseBrowser(function () {
-            console.log('Window will be closed');
-          }, function (error) {
-            console.log('There is an error');
-            console.log(error);
-          });
-        });
-      });
-    
-      
-      
-    }
-    </script>
-    
-    <script src="https://code.jquery.com/jquery-2.2.1.min.js"
-            integrity="sha256-gvQgAFzTH6trSrAWoH1iPo9Xc96QxSZ3feW6kem+O00="
-            crossorigin="anonymous"></script>
-  </body>
+</body>
 </html>
